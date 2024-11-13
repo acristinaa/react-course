1a7396db7503d230dbfd3d8662a75d16bcae5538
Cristina Ayala <crisstinaayala777@gmail.com>
2024-11-13 15:16:34 -0800

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Post from './components/Post'
+import Post from "./components/Post";
 
-function App(){
-  return <Post />;
+function App() {
+  return (
+    <main>
+      <Post author="Cristina" body="React.js is awesome!"/>
+      <Post author="Sara" body="Check it out"/>
+    </main>
+  );
 }
 
 export default App;
